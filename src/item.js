87d5727665b423f8c4d54dd1c0c92ec0522c6f46
Sean Gregory <chiefsgreg2622@gmail.com>
2024-09-09 19:21:32 -0400

    Added more details

--- src/item.js
+++ src/item.js
@@ -22,10 +22,33 @@ document.getElementsByClassName("contentTitle")[0].innerHTML = data.title;
 let description = document.getElementsByClassName("contentDesc")[0];
 description.innerHTML = "";
 
+let linkNum = 0;
 data.description.forEach(desc => 
 {
     let p = document.createElement("p");
     p.innerHTML = desc;
+    if (data.descriptionLinks != null)
+    {
+        if (p.innerHTML.indexOf("LINK") != -1)
+        {
+            let splitter = p.innerHTML.split("LINK")
+            console.log(splitter);
+            // <a class='unstyledLink'>VikramSinghMTL</a>
+            let a = document.createElement("a");
+            a.classList.add("unstyledLink");
+            let link = data.descriptionLinks[linkNum];
+            a.href = link;
+            let linkSplitter = link.split("/");
+            a.innerHTML = linkSplitter[linkSplitter.length - 1];
+            a.style.color = jsonColours.button;
+            a.style.fontWeight = "1000";
+            console.log();
+            p.innerHTML = splitter[0];
+            p.appendChild(a);
+            p.innerHTML += splitter[1];
+            linkNum++;
+        }
+    }
     description.appendChild(p);
 
     let br = document.createElement("br");
@@ -37,7 +60,7 @@ document.getElementsByClassName("contentBox")[0].style.backgroundImage = `url('.
 function formulateButton()
 {
     let button = document.getElementsByTagName("button")[0];
-
+    let p;
     switch(data.type)
     {
         case "film":
@@ -47,8 +70,26 @@ function formulateButton()
             let minutes = Math.floor(time / 60);
             let seconds = (time % 60 < 10) ? "0" + time % 60 : time % 60;
 
-            let p = document.createElement("p");
+            p = document.createElement("p");
             p.innerHTML = `Length: ${minutes}:${seconds}`
+            p.style.color = jsonColours.buttonBorder;
+            p.style.fontSize = "36px";
+            description.appendChild(p);
+        break;
+        case "game":
+            console.log("HI")
+            button.innerHTML = "Play now!"
+            button.addEventListener("click", () => { window.location.href = data.source })
+            
+            p = document.createElement("p");
+
+            p.innerHTML = "Languages Used:";
+            data.languages.forEach(language => 
+            {
+                p.innerHTML += ` ${language},`
+            });
+
+            p.innerHTML = p.innerHTML.substring(0, p.innerHTML.length - 1);
             p.style.color = jsonColours.buttonBorder;
             p.style.fontSize = "36px";
             description.appendChild(p);
